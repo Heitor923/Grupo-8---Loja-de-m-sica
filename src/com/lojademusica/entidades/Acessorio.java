cd6d572d37b2e09a7c2a9eca6657ea52da04f7a2
Ian Clauhs <ianclauhs@gmail.com>
2025-09-22 16:08:41 -0300

    Adicionei o commit Acessorio.java

--- src/com/lojademusica/entidades/Acessorio.java
+++ src/com/lojademusica/entidades/Acessorio.java
@@ -1,2 +1,14 @@
-!
-hjg
+public class Acessorio extends Produto {
+    private String material;
+
+    public Acessorio(int codigo, String nome, double preco, Categoria categoria, String material) {
+        super(codigo, nome, preco, categoria);
+        this.material = material;
+    }
+public String getMaterial() {
+        return material;
+    }
+    @Override
+    public void exibirDetalhes() {
+        System.out.printf("Acessório: %s - Material: %s\n", getNome(), getMaterial());
+    }
