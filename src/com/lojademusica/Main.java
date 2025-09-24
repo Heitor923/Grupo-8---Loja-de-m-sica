5ba37a91e9d97f35723d4c766e3732e9429f5981
Heitor Costa Silva <heitorcostasilva5@gmail.com>
2025-09-23 23:53:43 -0300

    Implementa funcionalidades de cadastro e consulta de produtos e vendas na classe Main

--- src/com/lojademusica/Main.java
+++ src/com/lojademusica/Main.java
@@ -1,11 +1,42 @@
 package com.lojademusica;
 
-import com.lojademusica.entidades.Funcionario;
-import com.lojademusica.entidades.Produto;
-import com.lojademusica.entidades.Venda;
-import com.lojademusica.entidades.Instrumento;
-import com.lojademusica.entidades.Acessorio;
-import com.lojademusica.entidades.Categoria;
+import com.lojademusica.entidades.*;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Funcionario f = new Funcionario("faea",  1);
 
 
+        Produto guitarra = new Instrumento(101, "Guitarra", 1500.0, Categoria.CORDAS, "Elétrica", "Fender");
+        Produto bateria = new Instrumento(102, "Bateria", 2500.0, Categoria.PERCUSSAO, "Acústica", "Yamaha");
+        Produto cordaExtra = new Acessorio(201, "Corda Extra", 50.0, Categoria.ACESSORIOS, "Aço");
+
+        f.cadastrarProduto(guitarra);
+        f.cadastrarProduto(bateria);
+        f.cadastrarProduto(cordaExtra);
+
+        System.out.println("Produtos Cadastrados");
+        f.consultarProduto(101).exibirDetalhes();
+        f.consultarProduto(102).exibirDetalhes();
+        f.consultarProduto(201).exibirDetalhes();
+
+   
+        Venda venda1 = f.gerarVenda(1);
+        venda1.adicionarProduto(guitarra, 1);
+        venda1.adicionarProduto(cordaExtra, 2);
+
+        Venda venda2 = f.gerarVenda(2);
+        venda2.adicionarProduto(bateria, 1);
+
+        System.out.println("\nVendas Realizadas");
+        Venda v1 = f.consultarVenda(venda1.getId());
+        System.out.println("Venda ID: " + v1.getId() + ", Valor Total: R$ " + v1.getValorTotal());
+
+        Venda v2 = f.consultarVenda(venda2.getId());
+        System.out.println("Venda ID: " + v2.getId() + ", Valor Total: R$ " + v2.getValorTotal());
+
+
+    }
+}
 
